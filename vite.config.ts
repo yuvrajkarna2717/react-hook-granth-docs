b2bb07fc61554b0218580bcaf83c82c29343ae91
Yuvraj Karna <yuvraj.karna@iquadra.com>
2025-08-08 15:55:56 +0530

    feat: added base in vite.config.ts fie

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import mdx from '@mdx-js/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/src/',
   plugins: [
     { enforce: 'pre', ...mdx() },
     react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ })
